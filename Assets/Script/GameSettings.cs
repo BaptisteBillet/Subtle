7b34532aa91e066618cd171634c6f2327d715939
Baptiste <b.billet@rubika-edu.com>
2015-06-05 01:45:24 +0200

    Start
    First commit

---
+++ Assets/Script/GameSettings.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSettings : MonoBehaviour {
+
+	#region Singleton
+	static private GameSettings s_Instance;
+	static public GameSettings instance
+	{
+		get
+		{
+			return s_Instance;
+		}
+	}
+
+
+
+	void Awake()
+	{
+		if (s_Instance == null)
+			s_Instance = this;
+		//DontDestroyOnLoad(this);
+	}
+	#endregion
+
+	#region members
+	public int m_NumberOfPlayers;
+	public int m_MaxNumberOfPlayers;
+	public int m_MinNumberOfPlayers;
+
+
+	#endregion
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
